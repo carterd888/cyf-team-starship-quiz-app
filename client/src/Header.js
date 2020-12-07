200b8730bf4b17b86d666c56d07c253bf85794cc
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-12-07 15:14:27 +0000

    added quiz button and logo to app.js

---
+++ client/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+      <div className="Header">
+        <img className="Logo" src="client\src\logo.png" />
+      </div>
+    );
+};
+
+export default Header;
