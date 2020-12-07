c0cb1ed174038f022f806d4c5b083e7d4aa4407a
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-12-07 01:46:19 +0000

    QuizExample is generated

---
+++ server/QuizExample.js
@@ -0,0 +1,51 @@
+let QuizExample = [
+    {
+    id:1,
+    moduleName:'Database',
+    quizQuestion: 'This is the text of the Question',
+    answerA: 'This is the first true answer',
+    answerB: 'This is the second wrong answer'
+    },
+
+    {
+    id:2,
+    moduleName:'Database',
+    quizQuestion: 'This is the text of the Question',
+    answerA: 'This is the first true answer',
+    answerB: 'This is the second wrong answer'
+    },
+    
+    {
+    id:3,
+    moduleName:'Database',
+    quizQuestion: 'This is the text of the Question',
+    answerA: 'This is the first true answer',
+    answerB: 'This is the second wrong answer'
+    },
+
+    {
+    id:5,
+    moduleName:'Database',
+    quizQuestion: 'This is the text of the Question',
+    answerA: 'This is the first true answer',
+    answerB: 'This is the second wrong answer'
+    },
+
+    {
+    id:6,
+    moduleName:'Database',
+    quizQuestion: 'This is the text of the Question',
+    answerA: 'This is the first true answer',
+    answerB: 'This is the second wrong answer'
+    },
+
+    {
+    id:7,
+    moduleName:'Database',
+    quizQuestion: 'This is the text of the Question',
+    answerA: 'This is the first true answer',
+    answerB: 'This is the second wrong answer'
+    }
+    
+
+];
