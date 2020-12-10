180d6db31de0b8fb3f223c9f2ccbb9d31cd95044
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-12-10 15:16:16 +0000

    added sql sheet and some database connections

--- server/db.js
+++ server/db.js
@@ -14,11 +14,13 @@ if (process.env.DATABASE_URL) { //it's set in Heroku
   }
 } else { //default local config
   config = {
-    host: 'localhost',
-    database: 'cyf_hotel',
-    password: '',
-    port: 5432
-  }
+    host: "ec2-54-170-190-29.eu-west-1.compute.amazonaws.com",
+    database: "d5rc1qa1b5k5eh",
+    user: "xscgoxtudvwele",
+    password:
+      "f8456fa3f695325a00315f24ff8f96d4c7e75dde02cd6eedc3ed81f3e27e3eb5",
+    port: 5432,
+  };
 }
 pool = new Pool(config)  
 
