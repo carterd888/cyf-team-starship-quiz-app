449a9cce1f01c05f82df1340c2418667157d77c4
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-12-10 14:50:49 +0000

    added sql file with data structure and live link to logo

--- client/src/GeneralPages/Header.js
+++ client/src/GeneralPages/Header.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Header = () => {
 	return (
-		<div className="Header">
-			<img className="Logo" src="./client/src/logo.png" />
-			<ul className='header-links'>
-				<Link to="/mentorpage">
-					<li>Mentor Access</li>
-				</Link>
-				<Link to="/studentpage">
-					<li>Student Access</li>
-				</Link>
-			</ul>
-		</div>
-	);
+    <div className="Header">
+      <img className="Logo" src="https://imgur.com/a/MJxKB3D" />
+      <ul className="header-links">
+        <Link to="/mentorpage">
+          <li>Mentor Access</li>
+        </Link>
+        <Link to="/studentpage">
+          <li>Student Access</li>
+        </Link>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
