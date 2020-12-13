45badacc74720dbcdb4f45110349f49f8ca4c0f4
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-12-13 17:34:41 +0000

    studentlogin page is created with SLQ connection

--- client/src/Mentor/MentorResults.js
+++ client/src/Mentor/MentorResults.js
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom';
 import Header from "../GeneralPages/Header";
 import Button from '../GeneralPages/Button';
 
-const StudentResults = () => {
+const MentorResults = () => {
     let [results, setResults] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:3100/api/studentresults") // Change to https://cyf-team-starship-quiz-app.herokuapp.com/api/studentresults
+        fetch("http://localhost:3100/api/mentorresults") // Change to https://cyf-team-starship-quiz-app.herokuapp.com/api/mentorresults
             .then(data => data.json())
             .then(jsonData => setResults(jsonData))
             .catch(e => console.log(e));
@@ -16,11 +16,12 @@ const StudentResults = () => {
     return (
         <div>
             <Header />
-            <h2>Your quiz results:</h2>
+            <h2>Students quiz results:</h2>
             <div>
                 <table>
                     <tr>
                         <th>Test</th>
+                        <th>Student Name</th>
                         <th>Score</th>
                     </tr> 
                     {
@@ -28,17 +29,18 @@ const StudentResults = () => {
                             return (
                                 <tr>
                                     <td>{r.quiz_name}</td>
+                                    <td>{r.student_name}</td>
                                     <td>{r.score}</td>
                                 </tr>);
                             })
                     }                  
                 </table>
             </div>
-            <Link to = "/studentpage">
-				<Button buttontext ='Go back to Student Page' />
+            <Link to = "/mentorpage">
+				<Button buttontext ='Go back to Mentor Page' />
 			</Link>
         </div>
     );
 }
 
-export default StudentResults;
+export default MentorResults;
