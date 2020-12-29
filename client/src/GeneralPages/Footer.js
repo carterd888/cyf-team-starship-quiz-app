530a788b32084070760f97fe67868865c25c0e32
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-12-29 22:22:02 +0000

    fixed typo on url

---
+++ client/src/GeneralPages/Footer.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+    <div>
+      <h4>Community:</h4>
+      <ul className="footer-links">
+        <li>
+          <a href="https://www.codeyourfuture.io" target="_blank">
+            Website
+          </a>
+        </li>
+        <li>
+          <a href="https://docs.codeyourfuture.io" target="_blank">
+            Documentation
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/codeyourfuture/" target="_blank">
+            GitHub
+          </a>
+        </li>
+      </ul>
+    </div>
+      <hr />
+      <div>
+      <h4>Socials:</h4>
+      <ul className="footer-links">
+        <li>
+          <a href="https://www.facebook.com/codeyourfuture.io" target="_blank">
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/codeyourfuture_" target="_blank">
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/CodeYourFuture" target="_blank">
+            Twitter
+          </a>
+        </li>
+      </ul>
+      </div>
+      <hr />
+      <div className="copyright">
+        <div>Copyright © 2020 CodeYourFuture</div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Footer;
