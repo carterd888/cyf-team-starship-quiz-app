8c0c8e544a365b363763363257feb5bf90c6811b
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-12-30 15:04:35 +0000

    Implement CSS, align the items

--- client/src/Mentor/MentorStyle.js
+++ client/src/Mentor/MentorStyle.js
@@ -10,6 +10,77 @@ const MentorStyle = createGlobalStyle`
     background: brown;
     font-family: 'Roboto', sans-serif;
   }
+ ${"" /* Mentor Login page */}
+  .mentor-login-form-div{
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .mentor-login-form{
+    display:flex;
+    margin-bottom:50px;
+    flex-direction: column;
+    justify-content: center;
+    
+  }
+  .mentor-login-input-label{
+    color: white;
+    margin-top: 50px;
+  }
+  .mentor-login-label{
+    text-align: center;
+  }
+ .button2{
+  background-color:#8E0D19;
+  align-self:center;
+  margin-left:25%;
+  margin-right:25%;
+  width:50%;
+  border-radius:5px;
+ }
+ .submit-button{
+  width:100%;
+  margin-left:0;
+ }
+
+${"" /* MentorPage */}
+
+ .mentor-page-buttons{
+  display:flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-content:space-around;
+  margin-top:50px;
+
+}
+.mentor-page-button{
+  margin-top:50px;
+}
+
+.quiz-name-form{
+  display:flex;
+  margin-bottom:50px;
+  flex-direction: column;
+  justify-content: center;
+}
+.quiz-name-label{
+  color: white;
+  margin-top: 10px;
+  margin-bottom:10px;
+  width: 50%;
+  margin-left:25%;
+  margin-right:25%;
+  text-align: center;
+}
+.quiz-name-input{
+  margin-bottom: 10px;
+  width: 50%;
+  margin-left:25%;
+  margin-right:25%;
+  border-radius:5px;
+}
+
+
 `;
 
 export default MentorStyle;
